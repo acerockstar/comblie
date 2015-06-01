f0649c6f29dbac035ba5b895ad87a13df3a7a4fa
Annie Cheng <ac962@cornell.edu>
2015-06-01 14:37:52 -0700

    Added color extensions

---
+++ Unify/ColorExtension.swift
@@ -0,0 +1,51 @@
+//
+//  ColorExtension.swift
+//  Unify
+//
+//  Created by Annie Cheng on 6/1/15.
+//  Copyright (c) 2015 Unify. All rights reserved.
+//
+
+import Foundation
+
+extension UIColor {
+    class func facebookBlue() -> UIColor {
+        // Hex: #3b5998, RGB: 59, 89, 152
+        return UIColor(red: CGFloat(59.0/255.0), green: CGFloat(89.0/255.0), blue: CGFloat(152.0/255.0), alpha: 1.0)
+    }
+    
+    class func instagramBlue() -> UIColor {
+        // Hex: #00aced, RGB: 0, 172, 237
+        return UIColor(red: CGFloat(0.0/255.0), green: CGFloat(172.0/255.0), blue: CGFloat(237.0/255.0), alpha: 1.0)
+    }
+    
+    class func tumblrBlue() -> UIColor {
+        // Hex: #32506d, RGB: 50, 80, 109
+        return UIColor(red: CGFloat(50.0/255.0), green: CGFloat(80.0/255.0), blue: CGFloat(109.0/255.0), alpha: 1.0)
+    }
+    
+    class func twitterBlue() -> UIColor {
+        // Hex: #517fa4, RGB: 81, 127, 164
+        return UIColor(red: CGFloat(81.0/255.0), green: CGFloat(127.0/255.0), blue: CGFloat(164.0/255.0), alpha: 1.0)
+    }
+    
+    class func vineGreen() -> UIColor {
+        // Hex: #00bf8f, RGB: 0, 191, 143
+        return UIColor(red: CGFloat(0.0/255.0), green: CGFloat(191.0/255.0), blue: CGFloat(143.0/255.0), alpha: 1.0)
+    }
+    
+    class func linkedinBlue() -> UIColor {
+        // Hex: #007bb6, RGB: 0, 123, 182
+        return UIColor(red: CGFloat(0.0/255.0), green: CGFloat(123.0/255.0), blue: CGFloat(182.0/255.0), alpha: 1.0)
+    }
+    
+    class func youtubeRed() -> UIColor {
+        // Hex: #bb0000, RGB: 187, 0, 0
+        return UIColor(red: CGFloat(187.0/255.0), green: CGFloat(0.0/255.0), blue: CGFloat(0.0/255.0), alpha: 1.0)
+    }
+    
+    class func googlePlusRed() -> UIColor {
+        // Hex: #dd4b39, RGB: 221, 75, 57
+        return UIColor(red: CGFloat(221.0/255.0), green: CGFloat(75.0/255.0), blue: CGFloat(57.0/255.0), alpha: 1.0)
+    }
+}
