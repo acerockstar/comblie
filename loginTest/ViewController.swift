70c55cc43eb9f067719c81882ed171da967bfddd
Sai Grandhi <sgrndh@gmail.com>
2015-07-12 17:41:44 -0500

    added facebook and parse sdk

---
+++ loginTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  loginTest
+//
+//  Created by Sai Grandhi on 9/12/15.
+//  Copyright (c) 2015 Sai Grandhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
