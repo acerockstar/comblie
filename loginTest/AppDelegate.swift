0dcddee18b9d6baae81da3e0819c64a52be8070c
Sai Grandhi <sgrndh@gmail.com>
2015-07-14 18:40:40 -0500

    implemented parse sdk

--- loginTest/AppDelegate.swift
+++ loginTest/AppDelegate.swift
@@ -17,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         Parse.setApplicationId("IibrqFEsqv6UpEEQ8wsIX0iHrWJ4bvF2PL3Lx8xu", clientKey: "vr7mTyWJKjh8y8nHprCGwua1GaxxU87NFVKZxHSY")
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         return true
     }
 
