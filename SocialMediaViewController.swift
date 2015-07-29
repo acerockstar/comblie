d051d2190ee25a4099f756e33c9636064e060b4a
Monu <Monu@Monus-Mac-mini.local>
2015-07-30 01:32:13 +0530

    Use  email id password custom view on social network view controller

--- SocialMediaViewController.swift
+++ SocialMediaViewController.swift
@@ -13,6 +13,9 @@ import Parse
 
 class SocialMediaViewController: UIViewController,WebServiceDelegate {
     
+    @IBOutlet weak var emailIdTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginPopupView: UIView!
     @IBOutlet weak var welcomeBackLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var twitterButton: UIButton!
@@ -29,6 +32,8 @@ class SocialMediaViewController: UIViewController,WebServiceDelegate {
     let container = CKContainer.defaultContainer()
     var publicDatabase: CKDatabase?
     var currentRecord: CKRecord?
+    
+    var CheckClickBtn : String!
 
      var window: UIWindow?
     override func viewDidLoad() {
@@ -72,14 +77,17 @@ class SocialMediaViewController: UIViewController,WebServiceDelegate {
             let thickString = NSAttributedString(string: "Sign Up", attributes: [NSFontAttributeName: mediumFont])
             signUpString.appendAttributedString(thickString)
         }
-        
+
         signUpButton.setAttributedTitle(signUpString, forState: .Normal)
         
     }
     @IBAction func LoginWithInstagram(sender: AnyObject) {
+        CheckClickBtn = "Instagram"
         instagramButton.backgroundColor = UIColor.comblieDarkPurple()
         instagramButton.setTitleColor(UIColor.backgroundLightGrey(), forState: .Normal)
-        
+        if loginPopupView.hidden == true {
+            loginPopupView.hidden = false
+        }
         if Reachability.isConnectedToNetwork() == true {
             LoginWithInstagram()
         } else {
@@ -87,6 +95,7 @@ class SocialMediaViewController: UIViewController,WebServiceDelegate {
         }
     }
     @IBAction func LoginWithTumblr(sender: AnyObject) {
+        CheckClickBtn = "Tumblr"
         tumblrButton.backgroundColor = UIColor.comblieDarkPurple()
         tumblrButton.setTitleColor(UIColor.backgroundLightGrey(), forState: .Normal)
         
@@ -98,6 +107,7 @@ class SocialMediaViewController: UIViewController,WebServiceDelegate {
     }
 
     @IBAction func LoginWithTwitter(sender: AnyObject) {
+        CheckClickBtn = "Twitter"
         twitterButton.backgroundColor = UIColor.comblieDarkPurple()
         twitterButton.setTitleColor(UIColor.backgroundLightGrey(), forState: .Normal)
         
@@ -109,14 +119,13 @@ class SocialMediaViewController: UIViewController,WebServiceDelegate {
     }
 
     @IBAction func LoginWithVine(sender: AnyObject) {
+        CheckClickBtn = "Vine"
         vineButton.backgroundColor = UIColor.comblieDarkPurple()
         vineButton.setTitleColor(UIColor.backgroundLightGrey(), forState: .Normal)
-        
-        if Reachability.isConnectedToNetwork() == true {
-            LoginWithVine()
-        } else {
-             alertTitle("No Internet Connection", message: "Make sure your device is connected to the internet.", btnTitle: "OK")
+        if loginPopupView.hidden == true {
+            loginPopupView.hidden = false
         }
+       
 
     }
     @IBAction func NewUserLogin(sender: AnyObject) {
@@ -131,7 +140,9 @@ class SocialMediaViewController: UIViewController,WebServiceDelegate {
         CheckValue = "1"
         Loader.showActivityIndicator(self.view)
         api.delegate=self
-        api.Login_With_Vine("Shailendragupta35@yahoo.com", Password: "Monu1987")
+        //Shailendragupta35@yahoo.com
+        //Monu1987
+        api.Login_With_Vine(emailIdTextField.text, Password:passwordTextField.text)
     }
 
     func VineUserInfo(userid : NSString){
@@ -207,20 +218,43 @@ class SocialMediaViewController: UIViewController,WebServiceDelegate {
                     var likeDouble  = paraDict.objectForKey("data")?.valueForKey("likeCount") as! Double
                     var loopDouble  = paraDict.objectForKey("data")?.valueForKey("loopCount") as! Double
                     var postDouble  = paraDict.objectForKey("data")?.valueForKey("postCount") as! Double
-
+                    
                     let VineObject = PFObject(className: "Vine")
-                    VineObject["avatarUrl"] = avatarUrlString
-                    VineObject["userId"] = userIds
-                    VineObject["username"] = userNameString
-                    VineObject["followerCount"] = followerDouble
-                    VineObject["followingCount"] = followingDouble
-                    VineObject["likeCount"] = likeDouble
-                    VineObject["loopCount"] = loopDouble
-                    VineObject["postCount"] = postDouble
-
-                    VineObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-                     println("Object has been saved.")
-                    }
+                    let query = PFQuery(className: "Vine")
+                    query.selectKeys(["userId"])
+                    query.findObjectsInBackgroundWithBlock({ (result : [AnyObject]?, error : NSError?) -> Void in
+                        
+                        if error == nil {
+                            
+                           if result?.count>0{
+                            query.whereKey("userId", equalTo: userIds)
+                             VineObject.setValue(avatarUrlString, forKey: "avatarUrl")
+                             VineObject.setValue(userIds, forKey: "userId")
+                             VineObject.setValue(userNameString, forKey: "username")
+                             VineObject.setValue(followerDouble, forKey: "followerCount")
+                             VineObject.setValue(followingDouble, forKey: "followingCount")
+                             VineObject.setValue(likeDouble, forKey: "likeCount")
+                             VineObject.setValue(loopDouble, forKey: "loopCount")
+                             VineObject.setValue(postDouble, forKey: "postCount")
+                             VineObject.save()
+                            println("Object has been Update.")
+                            }
+                           else{
+                            
+                            VineObject["avatarUrl"] = avatarUrlString
+                            VineObject["userId"] = userIds
+                            VineObject["username"] = userNameString
+                            VineObject["followerCount"] = followerDouble
+                            VineObject["followingCount"] = followingDouble
+                            VineObject["likeCount"] = likeDouble
+                            VineObject["loopCount"] = loopDouble
+                            VineObject["postCount"] = postDouble
+                            VineObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+                                println("Object has been saved.")
+                            }
+                            }
+                        }
+                   })
                 }
                 else{
                    alertTitle("No Internet Connection", message: "Make sure your device is connected to the internet.", btnTitle: "OK")
@@ -235,6 +269,23 @@ class SocialMediaViewController: UIViewController,WebServiceDelegate {
 //                    InstagramUser_Info(idSting)
 //                }
 //            }
+    }
+    @IBAction func LoginClick(sender: AnyObject) {
+        
+        if emailIdTextField.text == "" || passwordTextField.text == "" {
+            alertTitle("alert!", message: "All field is requird", btnTitle: "OK")
+        }
+        else {
+            
+            if CheckClickBtn == "Vine" {
+                
+                if Reachability.isConnectedToNetwork() == true {
+                    LoginWithVine()
+                } else {
+                    alertTitle("No Internet Connection", message: "Make sure your device is connected to the internet.", btnTitle: "OK")
+                }
+            }
+        }
     }
     func alertTitle(title :String, message:String,btnTitle:String){
         var alert = UIAlertView(title: title, message:message, delegate: nil, cancelButtonTitle: btnTitle)
