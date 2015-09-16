6e4a8329671a0fbd7fa85b0f7186b8f8b7a1ad76
Maxime Defauw <maxime.defauw@gmail.com>
2015-09-16 21:43:59 +0200

    ChatBubble UI

---
+++ ChatBubble.swift
@@ -0,0 +1,21 @@
+//
+//  ChatBubble.swift
+//  
+//
+//  Created by Maxime Defauw on 16/09/15.
+//
+//
+
+import UIKit
+
+class ChatBubble: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Unify/shared/feed/ChatBubble.swift
@@ -0,0 +1,70 @@
+//
+//  ChatBubble.swift
+//  
+//
+//  Created by Maxime Defauw on 16/09/15.
+//
+//
+
+import UIKit
+
+@IBDesignable class ChatBubble: UIView {
+    
+    var view: UIView!
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBInspectable var image: UIImage? {
+        get {
+            return profileImage.image
+        }
+        set(image) {
+            profileImage.image = image
+        }
+    }
+    
+    var nibName: String = "ChatBubble"
+    
+    // init
+    
+    override init(frame: CGRect) {
+        // set properties
+        super.init(frame: frame)
+        
+        // Set anything that uses the view or visible bounds
+        setup()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        // set properties
+        super.init(coder: aDecoder)
+        
+        // Set anything that uses the view or visible bounds
+        setup()
+    }
+    func setup() {
+        view = loadViewFromNib()
+        
+        view.frame = bounds
+        view.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+        
+        addSubview(view)
+    }
+    
+    func loadViewFromNib() -> UIView {
+        
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: nibName, bundle: bundle)
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        
+        return view
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
