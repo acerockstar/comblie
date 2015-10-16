96f7710d425ed8d07380a5a8e4457ac46285d0eb
Mehmet Bajin <bajin.mehmet@gmail.com>
2015-10-15 19:45:42 -0500

    add vine feed

--- server/lib/networks/vine.js
+++ server/lib/networks/vine.js
@@ -13,14 +13,37 @@ var enums = require('../enums');
 
 // MARK: API
 
-
+var Vineapple = require('vineapple');
 
 // MARK: Feed
 
 exports.feedInitial = function (signature) {
   var promise = new rsvp.Promise(function (resolve, reject) {
-    // TODO
-    resolve({});
+    var vine = new Vineapple({
+      key: signature.access_token,
+      id: signature.id
+    });
+
+    vine.user(signature.id, function (error, posts) {
+      if (error) {
+        resolve(null);
+      }
+      else {
+        vine.likes(signature.id, function (error, likes) {
+          if (error) {
+            resolve({
+              posts: posts
+            });
+          }
+          else {
+            resolve({
+              posts: posts,
+              likes: likes
+            });
+          }
+        });
+      }
+    });
   });
 
   return promise;
