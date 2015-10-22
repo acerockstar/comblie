1f14002a1754987cfdd54c131146188ad602cade
Mehmet Bajin <bajin.mehmet@gmail.com>
2015-10-22 12:54:25 -0500

    entering email navigates to early-access.html now

--- website/js/comblie.js
+++ website/js/comblie.js
@@ -9,3 +9,11 @@ comblie.getParameterByName = function (name) {
       results = regex.exec(location.search);
   return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
+
+comblie.formatKeyIn = function (key) {
+  return key.replace(/\./g, '<DOT>');
+};
+
+comblie.formatKeyOut = function (key) {
+  return key.replace(/<DOT>/g, '.');
+};
